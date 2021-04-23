4a586479a256df79028ad9087f633cd348818d5f
Passive14 <marcusjhomann@outlook.com>
2021-04-23 09:31:44 +0200

    groovy test

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -4,15 +4,15 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
-            }            
+                git url: 'https://github.com/Passive1412/jgsu-spring-petclinic.git', branch: 'main'
+            }
         }
         stage('Build') {
             steps {
                 sh './mvnw clean package'
-                //sh 'false' // true
+            //sh 'false' // true
             }
-        
+
             post {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
@@ -20,10 +20,10 @@ pipeline {
                 }
                 changed {
                     emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
+                        body: "Please go to ${BUILD_URL} and verify the build",
+                        attachLog: true,
+                        compressLog: true,
+                        to: 'test@jenkins',
                         recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
